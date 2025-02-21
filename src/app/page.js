29a17edc83d46178f94f3438a5bc854d0a7ecaf2
FEM12 <victormarroquin469@gmail.com>
2025-02-21 14:42:18 -0600

    Diseño preliminar

--- src/app/page.js
+++ src/app/page.js
@@ -3,93 +3,68 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <div className={styles.chat_container}>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+          <div className={styles.chat_header}>
+            <div className={styles.container_logo}>
+              <img className={styles.chatbot_logo} src="img/bot-logo.jpg"/>
+            </div>
+            <div className={styles.chat_name}>
+              <p>Chatbot UDB</p>
+              <p><span className={styles.dot}>&#9679;</span> En Linea</p>
+            </div>
+          </div>
+
+          <div className={styles.chat_body}>
+            <span className={styles.my_message}>
+              <p>¿Cuál es el calendario académico para este semestre?</p>
+            </span>
+            <span className={styles.bot_message}>
+              <p>
+                ontrary to popular belief, Lorem Ipsum is not simply random text.
+                It has roots in a piece of classical Latin literature from 45 BC,
+                making it over 2000 years old. Richard McClintock, a Latin 
+                professor at Hampden-Sydney College in Virginia, looked up one of 
+                the more obscure Latin words, consectetur, from a Lorem Ipsum 
+                passage, and going through the cites of the word in classical 
+                literature, discovered the undoubtable source. Lorem Ipsum 
+                comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum 
+                et Malorum" (The Extremes of Good and Evil) by Cicero, written in
+                45 BC. This book is a treatise on the theory of ethics, very popular 
+                during the Renaissance. The first line of Lorem Ipsum, "Lorem 
+                ipsum dolor sit amet..", comes from a line in section 1.10.32.
+              </p>
+            </span>
+            <span className={styles.my_message}>
+              <p>¿Hay algún evento importante en el campus esta semana?</p>
+            </span>
+            <span className={styles.bot_message}>
+              <p>
+                ontrary to popular belief, Lorem Ipsum is not simply random text.
+                It has roots in a piece of classical Latin literature from 45 BC,
+                making it over 2000 years old. Richard McClintock, a Latin 
+                professor at Hampden-Sydney College in Virginia, looked up one of 
+                the more obscure Latin words, consectetur, from a Lorem Ipsum 
+                passage, and going through the cites of the word in classical 
+                literature, discovered the undoubtable source. Lorem Ipsum 
+                comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum 
+                et Malorum" (The Extremes of Good and Evil) by Cicero, written in
+                45 BC. This book is a treatise on the theory of ethics, very popular 
+                during the Renaissance. The first line of Lorem Ipsum, "Lorem 
+                ipsum dolor sit amet..", comes from a line in section 1.10.32.
+              </p>
+            </span>
+          </div>
+
+          <div className={styles.chat_footer}>
+            <input type="text" placeholder="Mensaje"></input>
+            <button>&#10148;</button>
+          </div>
+          
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
